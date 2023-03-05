480bd56908c917c3dfd4f9a91972c58bf2e04332
MrMondego <mondegosol1@mail.ru>
2023-03-05 11:25:13 +0300

    deployable to gh pages

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -9,7 +9,7 @@ export default function Layout() {
             <img src={logo} height="80vmin" alt="logo" />
             <ul>
                 <li>
-                    <NavLink to='/'>Главная</NavLink>
+                    <NavLink to='/react_labs'>Главная</NavLink>
                 </li>
             </ul>
         </header>
