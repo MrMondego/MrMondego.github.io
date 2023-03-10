ae9156ead5d369721ca43be7eac84ebcdb7bbf8a
MrMondego <mondegosol1@mail.ru>
2023-03-10 14:38:16 +0300

    some styles

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -1,15 +1,16 @@
 import React from 'react';
 import { Outlet, NavLink } from 'react-router-dom';
 import logo from '../logo.svg';
+import '../css/layout.css'
 
 export default function Layout() {
     return(
         <>
         <header className='layout'>
             <img src={logo} width="77vmax" alt="logo" />
-            <ul>
+            <ul className='link-hover-center'>
                 <li>
-                    <NavLink to='/'>Главная</NavLink>
+                    <NavLink to='/' id="main-link">Главная</NavLink>
                 </li>
             </ul>
         </header>
