4140c9d5b6b84ac766810f0d384925e2f928c2a9
MrMondego <mondegosol1@mail.ru>
2023-03-17 15:28:46 +0300

    lab3, task 1 (no formatted input)

--- src/pages/lab3/PhoneInput.jsx
+++ src/pages/lab3/PhoneInput.jsx
@@ -1,7 +1,7 @@
 // import "../../lib";
 import ReactIntlTelInput from 'react-intl-tel-input-v2';
 import 'intl-tel-input/build/css/intlTelInput.css';
-import { useEffect, useState } from 'react';
+import { useState } from 'react';
 
 
 // const numberPatterns = [
@@ -14,29 +14,60 @@ import { useEffect, useState } from 'react';
 // ];
 
 function Operator(props) {
-    console.log("Operator", props.num)
-    switch(props.num) {
-        default: return(<></>)
-        case 1:
-            return(<>
-                <label><input type="radio" value="МТС" /> МТС</label>
-                <label><input type="radio" value="A1" /> А1</label>
-                <label><input type="radio" value="life:)" /> life:)</label>
-            </>)
-    }
+    console.log("Operator", props)
+    if(props.title.includes("Belarus"))
+        return(<>
+            <label htmlFor="operator">Выбрать оператора:</label>
+            <label><input type="radio" value="МТС" name="operator" /> МТС</label>
+            <label><input type="radio" value="A1" name="operator" /> А1</label>
+            <label><input type="radio" value="life:)" name="operator" /> life:)</label>
+        </>)
+    if(props.title.includes("Russia"))
+        return(<>
+            <label htmlFor="operator">Выбрать оператора:</label>
+            <label><input type="radio" value="МТС" name="operator" /> МТС</label>
+            <label><input type="radio" value="Megafon" name="operator" /> Мегафон</label>
+            <label><input type="radio" value="Bilane" name="operator" /> Билайн</label>
+            <label><input type="radio" value="tele2" name="operator" /> Tele2</label>
+        </>)
+    if(props.title.includes("Ukraine"))
+        return(<>
+            <label htmlFor="operator">Выбрать оператора:</label>
+            <label><input type="radio" value="Lifecell" name="operator" /> Lifecell</label>
+            <label><input type="radio" value="Vodafone" name="operator" /> Vodafone</label>
+            <label><input type="radio" value="Kievstar" name="operator" /> Київстар</label>
+        </>)
+    if(props.title.includes("Poland"))
+        return(<>
+            <label htmlFor="operator">Выбрать оператора:</label>
+            <label><input type="radio" value="Orange" name="operator" /> Orange</label>
+            <label><input type="radio" value="Play" name="operator" /> Play</label>
+            <label><input type="radio" value="Plus" name="operator" /> Plus</label>
+            <label><input type="radio" value="T-mobile" name="operator" /> T-mobile</label>
+        </>)
+    if(props.title.includes("Lithuania"))
+        return(<>
+            <label htmlFor="operator">Выбрать оператора:</label>
+            <label><input type="radio" value="Telia" name="operator" /> Telia</label>
+            <label><input type="radio" value="Bite" name="operator" /> Bite</label>
+            <label><input type="radio" value="Tele2" name="operator" /> Tele2</label>
+        </>)
+    if(props.title.includes("Latvia"))
+        return(<>
+            <label htmlFor="operator">Выбрать оператора:</label>
+            <label><input type="radio" value="LMT" name="operator" /> LMT</label>
+            <label><input type="radio" value="Tele2" name="operator" /> Tele2</label>
+            <label><input type="radio" value="Bite" name="operator" /> Bite</label>
+        </>)
+    return(<label>Нет операторов для страны с таким номером</label>)
 }
 
 export default function PhoneInput() {
     // console.log(.includes("China"));
-    const [num, setNum] = useState(0);
-    useEffect(() => {
-        const title = document.getElementsByClassName("iti__selected-flag")[0].attributes.getNamedItem("title").value;
-        if(title.includes("Belarus"))
-            setNum(1);
-    }, [])
+    const [title, setTitle] = useState('');
     return(<div className="phone-input">
         <label>Ваш телефон</label>
-        <ReactIntlTelInput />
-        <Operator num={num} />
+        <ReactIntlTelInput onChange={()=>{setTitle(document.getElementsByClassName("iti__selected-flag")[0].attributes.getNamedItem("title").value);}} />
+        <Operator title={title} />
     </div>)
 }
