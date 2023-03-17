1b8e81813287435c9da641591ef57b0e5950aafc
MrMondego <mondegosol1@mail.ru>
2023-03-17 15:45:32 +0300

    lab3, task 1 styles

--- src/pages/lab3/Lab3.jsx
+++ src/pages/lab3/Lab3.jsx
@@ -1,6 +1,7 @@
 import { useState } from 'react'
 import PhoneInput from 'react-phone-input-2'
 import 'react-phone-input-2/lib/style.css'
+import '../../css/lab3.css'
 
 function Operator(props) {
     console.log("Operator", props)
