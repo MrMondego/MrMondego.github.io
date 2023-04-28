716b04e253135a60f7c0629ce5d7b92554642d00
MrMondego <mondegosol1@mail.ru>
2023-04-28 16:22:58 +0300

    page title lab 7

--- src/pages/lab7/Lab7.jsx
+++ src/pages/lab7/Lab7.jsx
@@ -1,6 +1,8 @@
+import { useEffect } from "react";
 import Scheduler from "./Scheduler";
 
 export default function Lab7() {
+   useEffect(() => {document.title = "Лабораторная №7"}, []);
    return(<>
       <Scheduler />
    </>)
